657042fb9b225c394aacb43e2370b861804f6665
ArlynAnnelie <marianavioneag@gmail.com>
2023-12-16 22:01:52 +0200

    I added the home page

---
+++ src/main/java/com/SpotifyClone/SpotifyClone/controller/SpotifyController.java
@@ -0,0 +1,14 @@
+package com.SpotifyClone.SpotifyClone.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SpotifyController {
+	
+	@GetMapping("/")
+	public String home() {
+		return "home";
+	}
+
+}
