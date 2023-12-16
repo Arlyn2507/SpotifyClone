0c12cf6302b56eee264c5d448443ec15228170b2
ArlynAnnelie <marianavioneag@gmail.com>
2023-12-16 21:54:39 +0200

    I added some files

---
+++ src/main/java/com/SpotifyClone/SpotifyClone/SpotifyCloneApplication.java
@@ -0,0 +1,13 @@
+package com.SpotifyClone.SpotifyClone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpotifyClone/SpotifyClone/SpotifyCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpotifyClone.SpotifyClone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
