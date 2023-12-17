049aa6fb278bad63a7e36f1d0a50ed3cd5feaea8
ArlynAnnelie <marianavioneag@gmail.com>
2023-12-17 12:38:05 +0200

    I changed something

--- src/main/java/com/webapp/SpotifyClone/repository/SongRepository.java
+++ src/main/java/com/webapp/SpotifyClone/repository/SongRepository.java
@@ -1,4 +1,4 @@
-package com.SpotifyClone.SpotifyClone.repository;
+package com.webapp.SpotifyClone.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
